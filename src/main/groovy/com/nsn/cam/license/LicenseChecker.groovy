951316dd9fa4a1ffa99abb310565cf828267a847
Akos Fabian <fabian.akos@gmail.com>
2014-05-01 17:11:01 +0200

    renaming pom file

--- src/main/groovy/com/nsn/cam/license/LicenseChecker.groovy
+++ src/main/groovy/com/nsn/cam/license/LicenseChecker.groovy
@@ -2,9 +2,8 @@ package com.nsn.cam.license
 
 import com.nsn.cam.license.DependencyCollector
 
-// TODO: rename to LicenseChecker
 class LicenseChecker { 
-	private final static String POM_FILE = 'effective-pom.xml' // TODO: use underscore instead
+	private final static String POM_FILE = 'effective_pom.xml' // TODO: use underscore instead
 	private final static String REGISTRY_FILE = 'license_registry.xml'
 	private final static String REPORT_FILE = 'license_report.xml'
 
