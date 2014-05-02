0761db9975a2a2119c209382f020f0db71daccf2
Akos Fabian <fabian.akos@gmail.com>
2014-05-02 20:06:19 +0200

    using setUp in test

--- src/test/groovy/com/nsn/cam/license/LicenseRegistryTest.groovy
+++ src/test/groovy/com/nsn/cam/license/LicenseRegistryTest.groovy
@@ -4,17 +4,19 @@ import groovy.util.GroovyTestCase
 
 class LicenseRegistryTest extends GroovyTestCase {
 
-	void testContains() {
+	private LicenseRegistry registry
+
+	void setUp() {
 		URL url = this.class.getResource("/com/nsn/cam/license/sample_license_registry.database")
 		File testRegistryFile = new File(url.getFile())
-		LicenseRegistry registry = new LicenseRegistry(testRegistryFile)
+		registry = new LicenseRegistry(testRegistryFile)
+	}
+
+	void testContains() {
 		assert registry.contains('junit:junit:4') 
 	}
 
 	void testContainsMajor() {
-		URL url = this.class.getResource("/com/nsn/cam/license/sample_license_registry.database")
-		File testRegistryFile = new File(url.getFile())
-		LicenseRegistry registry = new LicenseRegistry(testRegistryFile)
 		assert registry.containsMajor('junit:junit:4.11') 
 	}
 }
