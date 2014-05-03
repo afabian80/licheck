d70c73d4393bb7745b9efe1f05ed045842a50c7f
Akos Fabian <fabian.akos@gmail.com>
2014-05-03 13:34:32 +0200

    generate report and its test

--- src/main/groovy/com/nsn/cam/license/LicenseReporter.groovy
+++ src/main/groovy/com/nsn/cam/license/LicenseReporter.groovy
@@ -1,5 +1,7 @@
 package com.nsn.cam.license
 
+import groovy.xml.MarkupBuilder
+
 class LicenseReporter {
 	File reportFile
 	def missingLicenses = [:]
@@ -15,5 +17,31 @@ class LicenseReporter {
 
 	void reportFound(dep, userProjects) {
 		okayLicenses[dep] = userProjects
+	}
+
+	String generateReport() {
+		def writer = new StringWriter()
+		def xml = new MarkupBuilder(writer)
+		xml.report() {
+			'missing'() {
+				missingLicenses.keySet().sort().each { key ->
+					'dependency'(name: "$key") {
+						missingLicenses[key].each { proj ->
+							'project'(name: "$proj")
+						}
+					}
+				}
+			}
+			'found'() {
+				okayLicenses.keySet().sort().each { key ->
+					'dependency'(name: "$key") {
+						okayLicenses[key].each { proj ->
+							'project'(name: "$proj")
+						}
+					}
+				}
+			}
+		}
+		return writer.toString()
 	}
 }
--- src/test/groovy/com/nsn/cam/license/LicenseReporterTest.groovy
+++ src/test/groovy/com/nsn/cam/license/LicenseReporterTest.groovy
@@ -21,4 +21,25 @@ class LicenseReporterTest extends GroovyTestCase {
 		reporter.reportFound(dep, ['project1', 'project2'])
 		assert reporter.okayLicenses.keySet().contains(dep)
 	}
+
+	void testGenerateReport() {
+		String missingDep = 'group1:artifact1:1.0'
+		String foundDep = 'group2:artifact2:2.0'
+		reporter.reportMissing(missingDep, ['project1', 'project2'])
+		reporter.reportFound(foundDep, ['project3', 'project4'])
+
+		String reportStr = reporter.generateReport()
+		println reportStr
+		def report = new XmlSlurper().parseText(reportStr)
+		def missing = report.missing
+		assert missing.size() == 1
+		assert missing.dependency.size() == 1
+		def elems = missing.dependency.findAll { it.@name == missingDep }
+		assert elems.size() == 1
+
+		def found = report.found
+		assert found.size() == 1
+		assert found.dependency.size() == 1
+		assert found.dependency.findAll { it.@name == foundDep }.size() == 1
+	}
 }
